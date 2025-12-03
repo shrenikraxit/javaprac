6b87bd4c84b02d1bd384c823a3dbe39655d39840
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2025-12-03 13:28:40 +0000

    comit

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+//java is a strongly typed language.
+
+class DataTypesExamples {
+    
+    int myint = 42;
+    double mydouble = 189.75;
+    char mychar = 'Q';
+    boolean mybool = false;
+}
+
+
+
+public class DataTypes { // tester class
+    public static void main(String [] args) { // code starts reading here
+
+        DataTypesExamples dt = new DataTypesExamples();
+
+        System.out.println("myInt: " + dt.myint);
+        
+    }
+
+}
