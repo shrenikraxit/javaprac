e4a8b09f378989848d762fe382742cc31a9539fb
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2025-12-04 14:38:05 +0000

    calc

---
+++ Math1.java
@@ -0,0 +1,43 @@
+class Calculator {
+
+    //write functions for addition subtractions multiplication and division
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int subtract(int a, int b) {
+        return a-b;
+
+    }
+
+    static int mult(int a, int b) {
+        return a*b;
+
+    }
+
+
+    static int div(int a, int b) {
+        return a/b;
+
+    }
+
+    static int mod(int a, int b) {
+        return a%b;
+
+    }
+
+    
+}
+
+
+
+public class Math1 {
+    public static void main(String[] args) {
+        
+    System.out.println(Calculator.add(10, 5));
+
+    }
+
+
+}
