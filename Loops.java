9d3e67a08495f7f9074136d719dbd266d667e17d
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2025-12-19 00:36:39 +0000

    final

---
+++ Loops.java
@@ -0,0 +1,3 @@
+public class Loops {
+  //my chromebook wasnt working so i couldnt take notes.  
+}
---
+++ LoopsChallenge1.java
@@ -0,0 +1,13 @@
+public class LoopsChallenge1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("foobar");
+            } else if (i % 3 == 0) {
+                System.out.println("foo");
+            } else if (i % 5 == 0) {
+                System.out.println("bar");
+            }
+        }
+    }
+}
