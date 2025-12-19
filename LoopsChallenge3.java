265fd9680073b2af3f7b251130da862457312a45
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2025-12-19 01:12:45 +0000

    finals

---
+++ LoopsChallenge3.java
@@ -0,0 +1,21 @@
+public class LoopsChallenge3 {
+
+}
+public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    int answer = (int) (Math.random() * 3) + 1;
+    int guess = 0;
+    
+    while (guess != answer) {
+        System.out.print("Guess a number between 1 and 3: ");
+        guess = scanner.nextInt();
+        
+        if (guess == answer) {
+            System.out.println("Correct answer!");
+        } else {
+            System.out.println("Wrong answer, try again.");
+        }
+    }
+    
+    scanner.close();
+}
