191f658b284ce5de6b20cb1d9cdc535a508cad5a
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2026-02-18 13:47:46 +0000

    final edit

---
+++ fundamentals/Personal.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class Personal {
+    public static void main(String[] args) {
+        String s = "19.95";
+        Double d = Double.parseDouble(s);
+        System.out.println(d);
+
+        int div = 10 / 4;
+        System.out.println(div);
+    }
+}
